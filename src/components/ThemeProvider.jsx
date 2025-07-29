c6bd7d2cb81f101a8912b1462c6d6e297f72e6b6
Abhishek Mallapareddy <abhishekreddy@aftl.co.uk>
2025-07-29 16:13:33 +0100

    fix: dark mode

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,41 +1,6 @@
 "use client";
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-const ThemeContext = createContext();
-
-export function useTheme() {
-  return useContext(ThemeContext);
-}
-
-export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('light');
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    if (storedTheme) {
-      setTheme(storedTheme);
-    } else {
-      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      setTheme(prefersDark ? 'dark' : 'light');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-    localStorage.setItem('theme', theme);
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
